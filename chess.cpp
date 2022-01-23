d6f14efccbfef2323ab73c30a50eeb2fd9270394
Eetu Rantala <rantala.eetu@gmail.com>
2022-01-23 21:53:01 +0200

    add iterative deepening and pv table

--- chess.cpp
+++ chess.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <climits>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
@@ -325,8 +326,84 @@ int mvv_lva[13][13] = {
 	0, 100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600,
 };
 
+const int max_depth = 64;
+
+/*
+      ================================
+            Triangular PV table
+      --------------------------------
+        PV line: e2e4 e7e5 g1f3 b8c6
+      ================================
+           0    1    2    3    4    5
+      
+      0    m1   m2   m3   m4   m5   m6
+      
+      1    0    m2   m3   m4   m5   m6 
+      
+      2    0    0    m3   m4   m5   m6
+      
+      3    0    0    0    m4   m5   m6
+       
+      4    0    0    0    0    m5   m6
+      
+      5    0    0    0    0    0    m6
+*/
+// PV length
+int pv_length[max_depth];
+
+// PV table
+int pv_table[max_depth][max_depth];
+
+// follow PV and score PV move
+int follow_pv, score_pv;
+
+// killer moves [id][ply]
+int killer_moves[2][max_depth];
+
+// history moves [piece][square]
+int history_moves[12][64];
+
+uint64_t nodes = 0;
+
+uint64_t quiesc_nodes = 0;
+
+int ply = 0;
+
+int max_ply = 0;
+
+// enable pv move scoring
+void enable_pv_scoring(Movelist &moves) {
+    // disable following pv
+    follow_pv = 0;
+
+    for (int i = 0; i < moves.count; i++) {
+        // make sure we hit pv
+        if (pv_table[0][ply] == moves.moves[i]) {
+            //enable move scoring
+            score_pv = 1;
+            // enable following pv
+            follow_pv = 1;
+
+        }
+    }
+}
+
 // score a move for move ordering purposes
 int score_move(Position &pos, int move) {
+
+    // if pv scoring enabled
+    if (score_pv) {
+        // make sure the move is pv move
+        if (pv_table[0][ply] == move) {
+            // disable move scoring
+            score_pv = 0;
+
+            //cout << "Current pv move: " <<  get_move_string(move) << " ply: " << ply << endl;
+            // prioritize pv move to sort it at the top
+            return 20000;
+        }
+    }
+
     // score captures
     if (decode_capture(move)) {
         int source_piece = pos.board[decode_source(move)];
@@ -338,16 +415,31 @@ int score_move(Position &pos, int move) {
         // cout << "source piece: " << Position::ascii_pieces[source_piece] << endl;
         // cout << "target piece: " << Position::ascii_pieces[target_piece] << endl;
         // cout << "score: " << mvv_lva[source_piece][target_piece] << endl;
-        return mvv_lva[source_piece][target_piece];
+
+        // add 10000 to add priority over quiet moves
+        return mvv_lva[source_piece][target_piece] + 10000;
     }
     // score quiet moves
     else {
-        return 0;
+
+        // score 1st killer move
+        if (killer_moves[0][ply] == move) {
+            return 9000;
+        }
+        // score 2nd killer move
+        else if (killer_moves[1][ply] == move) {
+            return 8000;
+        }
+        // score history move
+        
     }
+    return 0;
 }
 
 // prints move scores of moves in movelist
 void print_move_scores(Position &pos, Movelist &moves) {
+    //enable move scoring
+    score_pv = 1;
     for (int i = 0; i < moves.count; i++) {
         cout << "move: " << get_move_string(moves.moves[i]) << " score: " << score_move(pos, moves.moves[i]) << endl;
     }
@@ -370,18 +462,53 @@ struct Compare_move_scores {
 // }
 
 void sort_moves(Position &pos, Movelist &moves) {
-    //vector<int> moves_vector;
-    //moves_vector.assign(moves.moves, moves.moves + moves.count);
-    sort(moves.moves, moves.moves + moves.count, Compare_move_scores(pos));
+    Movelist move_scores;
+
+    for (int i = 0; i < moves.count; i++) {
+        move_scores.add_move(score_move(pos, moves.moves[i]));
+    }
+
+    int move, move_score, j;
+    for (int i = 1; i < moves.count; i++)
+    {
+        move = moves.moves[i];
+        move_score = move_scores.moves[i];
+        j = i - 1;
+ 
+        while (j >= 0 && move_scores.moves[j] < move_score)
+        {
+            moves.moves[j + 1] = moves.moves[j];
+            move_scores.moves[j + 1] = move_scores.moves[j];
+            j = j - 1;
+        }
+        moves.moves[j + 1] = move;
+        move_scores.moves[j + 1] = move_score;
+    }
+
+    // // loop over current move within a move list
+    // for (int current_move = 0; current_move < moves.count; current_move++)
+    // {
+    //     // loop over next move within a move list
+    //     for (int next_move = current_move + 1; next_move < moves.count; next_move++)
+    //     {
+    //         // compare current and next move scores
+    //         if (move_scores[current_move] < move_scores[next_move])
+    //         {
+    //             // swap scores
+    //             int temp_score = move_scores[current_move];
+    //             move_scores[current_move] = move_scores[next_move];
+    //             move_scores[next_move] = temp_score;
+                
+    //             // swap moves
+    //             int temp_move = moves.moves[current_move];
+    //             moves.moves[current_move] = moves.moves[next_move];
+    //             moves.moves[next_move] = temp_move;
+    //         }
+    //     }
+    // }
+
+    //sort(moves.moves, moves.moves + moves.count, Compare_move_scores(pos));
 }
-
-uint64_t nodes = 0;
-
-uint64_t quiesc_nodes = 0;
-
-int ply = 0;
-
-int max_ply = 0;
 
 int quiescence_search(Position &pos, int alpha, int beta) {
     // get lower bound score
@@ -433,8 +560,18 @@ int quiescence_search(Position &pos, int alpha, int beta) {
     return alpha;
 }
 
-// color +1 for white, -1 for black
+int leftmost = 1;
+
+// negamax search with alpha-beta pruning
 int negamax(Position &pos, int depth, int alpha, int beta) {
+    // init pv length
+    pv_length[ply] = ply;
+
+    // ensure no overflow of arrays depending on max_depth
+    if (ply > max_depth - 1) {
+        return evaluate_position(pos);
+    }
+
     if (depth == 0) {
         //nodes += 1;
         return quiescence_search(pos, alpha, beta);
@@ -448,9 +585,9 @@ int negamax(Position &pos, int depth, int alpha, int beta) {
     int king_attacked = is_square_attacked(pos, pos.king_squares[pos.side], !pos.side);
 
     // are we in check? if so, we want to search deeper
-	if (king_attacked) {
-        depth++;
-    }
+	// if (king_attacked) {
+    //     depth++;
+    // }
 
     // if no legal moves
     if (moves.count == 0) {
@@ -458,7 +595,7 @@ int negamax(Position &pos, int depth, int alpha, int beta) {
         if (king_attacked) {
             //cout << "found mate at ply " << ply << endl;
             //nodes += 1;
-            return -50000 + ply;
+            return -49000 + ply;
         }
         // king is not in check - stalemate
         else {
@@ -468,6 +605,12 @@ int negamax(Position &pos, int depth, int alpha, int beta) {
         }
     }
 
+    // if we are following pv line
+    if (follow_pv) {
+        // enable pv move scoring
+        enable_pv_scoring(moves);
+    }
+
     // sort moves
     sort_moves(pos, moves);
 
@@ -475,14 +618,29 @@ int negamax(Position &pos, int depth, int alpha, int beta) {
     Position copy = pos;
 
     for (int i = 0; i < moves.count; i++) {
+        // if (leftmost) {
+        //     print_move_scores(pos, moves);
+        //     cout << endl;
+        // }
         if (pos.make_move(moves.moves[i])) {
             nodes++;
             ply++;  
             //value = max(value, -negamax(pos, depth - 1, -beta, -alpha));
+            
             value = -negamax(pos, depth - 1, -beta, -alpha);
+            leftmost = 0;
             ply--;
             //alpha = max(alpha, value);
+            
+            // fail hard beta cutoff
             if (value >= beta) {
+                // for quiet moves
+                if (decode_capture(moves.moves[i]) == 0) {
+                    // store killer moves
+                    killer_moves[1][ply] = killer_moves[0][ply];
+                    killer_moves[0][ply] = moves.moves[i];
+                }
+
                 return beta;
             }
             // restore board state
@@ -492,6 +650,17 @@ int negamax(Position &pos, int depth, int alpha, int beta) {
             // }
             if (value > alpha) {
                 alpha = value;
+                // add pv move to pv table
+                pv_table[ply][ply] = moves.moves[i];
+
+                // copy deeper pv line
+                for (int next_ply = ply + 1; next_ply < pv_length[ply + 1]; next_ply++) {
+                    // copy deeper pv move to current table
+                    pv_table[ply][next_ply] = pv_table[ply + 1][next_ply];
+                }
+                
+                // increment pv length for current ply
+                pv_length[ply] = pv_length[ply + 1];
             }
         }
     }
@@ -501,74 +670,44 @@ int negamax(Position &pos, int depth, int alpha, int beta) {
 
 // search position for the best move
 void search_position(Position &pos, int depth) {
-    // best move placeholder
+    int score = 0;
+
+    // reset nodes counters
     nodes = 0;
     quiesc_nodes = 0;
-    int value = 0;
-    int best_move = 0;
-    int best_value = -500000;
 
-    // color for negamax calculation
-    //int color = pos.side == white ? 1 : -1;
+    follow_pv = 0;
+    score_pv = 0;
 
-    Movelist moves;
-    generate_legal_moves(pos, moves);
-
-    sort_moves(pos, moves);
-
-    // copy board state
-    Position copy = pos;
+    // clear helper data structures for search
+    memset(killer_moves, 0, sizeof(killer_moves));
+    memset(history_moves, 0, sizeof(history_moves));
+    memset(pv_table, 0, sizeof(pv_table));
+    memset(pv_length, 0, sizeof(pv_length));
 
     // start timing
     auto start = chrono::high_resolution_clock::now();
 
     // iterative deepening
-    //for (int iter_depth = 1; iter_depth <= depth; iter_depth++) {
-
-        // go through legal moves
-        for (int i = 0; i < moves.count; i++) {
-            if (pos.make_move(moves.moves[i])) {
-                nodes++;
-                //cout << i << endl;
-                ply++;
-                value = -negamax(pos, depth - 1, -500000, 500000);
-                ply--;
-                // restore board state
-                pos = copy;
-
-                if (value > best_value) {
-                    best_move = moves.moves[i];
-                    best_value = value;
-                }
-            }
-        }
+    for (int current_depth = 1; current_depth <= depth; current_depth++) {
+        follow_pv = 1;
+        leftmost = 1;
+        // find best move within a given position
+        score = negamax(pos, current_depth, -500000, 500000);
         // stop clock
         auto stop = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
 
-        // info depth 2 score cp 214 time 1242 nodes 2124 nps 34928
-        cout << "info depth " << depth << " score cp " << best_value << " nodes " << nodes << " nps " << max(0, int(1000.0 * nodes / duration.count())) << " pv " << endl;
-    //}
-    cout << "bestmove " << Position::square_to_coord[decode_source(best_move)] << Position::square_to_coord[decode_target(best_move)];
-    if (decode_promotion(best_move)) {
-        int promoted_piece = decode_promotion(best_move);
-        // promoted to queen
-        if ((promoted_piece == Q || promoted_piece == q))
-            cout << 'q';
-        
-        // promoted to rook
-        else if ((promoted_piece == R || promoted_piece == r))
-            cout << 'r';
-        
-        // promoted to bishop
-        else if ((promoted_piece == B || promoted_piece == b))
-            cout << 'b';
-        
-        // promoted to knight
-        else if ((promoted_piece == N || promoted_piece == n))
-            cout << 'n';
+        // info depth 2 score cp 214 time 1242 nodes 2124 nps 34928 pv e2e4 e7e5
+        cout << "info depth " << current_depth << " score cp " << score << " nodes " << nodes << " nps " << uint64_t(1000000.0 * nodes / duration.count()) << " pv ";
+        for (int i = 0; i < pv_length[0]; i++) {
+            cout << get_move_string(pv_table[0][i]) << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
+
+    
+    cout << "bestmove " << get_move_string(pv_table[0][0]) << endl; 
 }
 
 ////////////
@@ -699,7 +838,7 @@ void parse_go(Position &pos, string command) {
         depth = stoi(command.substr(index + 6));
     }
     else {
-        depth = 4;
+        depth = 5;
     }
 
     // todo time control
