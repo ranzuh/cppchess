dae5cd854b523d58602db0beb9654c35bb975861
Eetu Rantala <rantala.eetu@gmail.com>
2022-05-10 11:37:51 +0300

    pass evaluation through tanh-function to get eval from -1 to 1

--- tdleaf.cpp
+++ tdleaf.cpp
@@ -10,7 +10,7 @@ extern dvector weights;
 
 void train(vector<Position> leaf_positions, int result, int side) {
     // setup stepsize alpha and vector delta to hold the changes to weights
-    double alpha = 0.0005;
+    double alpha = 1;
     double lambda = 0.7;
     dvector delta(380, 0.0);
     dvector trace(380, 0.0);
@@ -22,27 +22,35 @@ void train(vector<Position> leaf_positions, int result, int side) {
 
         // eval current state with features
         double eval_state = evaluate_features(leaf_positions[i], features);
+        double tanh_eval_state = tanh(0.00255*eval_state);
 
         // eval next state. If its terminal its eval is the reward or matescore etc.
         double eval_next = 0.0;
+        double tanh_eval_next = 0.0;
         if (i == leaf_positions.size()-1) {
-            //eval_next = result * 2000;
-            eval_next = eval_state;
+            tanh_eval_next = result;
+            // eval_next = eval_state;
         }
         else {
             dvector features_next = extract_features(leaf_positions[i+1]);
             eval_next = evaluate_features(leaf_positions[i+1], features_next);
+            tanh_eval_next = tanh(0.00255*eval_next);
         }
         cout << "eval " << eval_state << endl;
         cout << "evalnext " << eval_next << endl;
+        cout << "tanh eval " << tanh_eval_state << endl;
+        cout << "tanh evalnext " << tanh_eval_next << endl;
 
         // increment eligibility trace
-        trace = vector_add(vector_scale(trace, lambda), features);
-        cout << "trace" << endl;
-        print_vector(trace);
+        dvector gradient(380, 0.0);
+        // nabla_w tanh(b*V) = (1-tanh^2(b*V)) * b*x
+        gradient = vector_scale(vector_scale(features, 0.00255), 1 - pow(tanh_eval_state, 2.0));
+        trace = vector_add(vector_scale(trace, lambda), gradient);
+        //cout << "trace" << endl;
+        //print_vector(trace);
 
         // calculate temporal difference
-        double td = eval_next - eval_state;
+        double td = tanh_eval_next - tanh_eval_state;
         cout << "td " << td << endl;
 
         // add trace scaled by temporal difference to weight vector delta
